a43f67dc930b8da397f7f9fd4c0b22ce0a36e204
陳可邦 <bensoncat99@gmail.com>
2021-01-16 00:41:50 +0800

    add some time parsing

--- gethtml.py
+++ gethtml.py
@@ -1,6 +1,7 @@
 import requests
+
 general = requests.get(
-    "https://nol.ntu.edu.tw/nol/coursesearch/search_for_03_co.php?current_sem=109-2&classarea=a&coursename=&teachername=&alltime=yes&allproced=yes&page_cnt=200&Submit22=%ACd%B8%DF")
+    "https://nol.ntu.edu.tw/nol/coursesearch/search_for_03_co.php?current_sem=109-2&classarea=a&coursename=&teachername=&alltime=yes&allproced=yes&page_cnt=2500&Submit22=%ACd%B8%DF")
 general.encoding = 'big5'
-with open("general.html", 'w') as f:
+with open("general2.html", 'w', encoding='UTF-8') as f:
     f.write(general.text)
--- parse.py
+++ parse.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(open("general.html"))
+# regex
+import re
 
+zh2num = {'日':0,'一':1,'二':2,'三':3,'四':4,'五':5,'六':6}
+class2num = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'A':11,'B':12,'C':13,'D':14}
+
+soup = BeautifulSoup(open("general2.html", encoding="UTF-8"),features="html.parser")
 table = soup.find_all('table')[7]
 # print(table)
 rows = table.find_all('tr')
@@ -10,17 +15,30 @@ for row in rows[1:]:
     columns = row.find_all('td')
     # print(columns)
     tmp = []
+    # 流水號
     tmp.append(columns[0].text)
+    # 課程編號
     tmp.append(columns[2].text)
+    # 課程名稱
     try:
         tmp.append(columns[4].a.text)
     except AttributeError:
-        tmp.append('爆炸拉')
+        tmp.append('[沒有名稱]')
+    # 學分數
     tmp.append(columns[6].text)
+    # 教師
     try:
         tmp.append(columns[9].a.text)
     except AttributeError:
-        tmp.append('爆炸拉')
+        tmp.append('[沒有老師]')
+    # 時間 地點
+    match = re.findall(r"([一二三四五六][0-9,ABCD]*)\(([^\(\)]*)\)",columns[11].text)
+    for m in match:
+        s = m.group(1)
+        day = zh2num[s[0]]
+        period = [class2num[x] for x in s[1:].split(',')]
+        # TODO: organize timetable
+
     print(tmp)
     courses.append(tmp)
 
